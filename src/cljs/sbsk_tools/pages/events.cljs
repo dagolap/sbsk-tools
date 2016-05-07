0167e016ee7e3b1a22c12a5029ea09ac0dc349ba
Dag Olav Prestegarden <dagolav@prestegarden.com>
2016-05-07 16:44:34 +0200

    Better date presentation. Now filtering on all clubs in the region

--- src/cljs/sbsk_tools/pages/events.cljs
+++ src/cljs/sbsk_tools/pages/events.cljs
@@ -1,14 +1,22 @@
 (ns sbsk-tools.pages.events
   (:require [reagent.core :as r]
             [cljs-time.core :as time]
+            [cljs-time.coerce :as timec]
             [ajax.core :refer [GET POST]]))
 
 (def view-configuration (r/atom {
-                                 :only-future?   true
-                                 :filtered-clubs ["Geita" "Sverr" "Tustn" "Yrjar" "Stoks" "VIGRA"]}))
+                                 :only-future?    true
+                                 :only-midtnorge? true
+                                 :filtered-clubs  ["Bøfjo" "Geita" "HEGIL" "Harøy" "HITRA" "Hunde"
+                                                   "GOMA" "Yrjar" "Levah" "Lyngs" "Malvi" "Molde"
+                                                   "Roan" "SALSN" "Stoks" "Surna" "Sverr" "Tustn"
+                                                   "Ulvun" "Verda" "Vestn" "VIGRA" "Volla" "Ålesu"]}))
 
 (defn event-in-filter-list [ev]
-  (some #(= (:organizer-short ev) %) (:filtered-clubs @view-configuration)))
+  (if (:only-midtnorge? @view-configuration)
+    (some #(= (:organizer-short ev) %) (:filtered-clubs @view-configuration))
+    true))
+
 
 (defn event-valid-according-to-view-config [ev]
   (if (:only-future? @view-configuration)
@@ -21,15 +29,47 @@
 (defn generate-shown-events [all-events shown-events]
   (reset! shown-events (filter event-valid-according-to-view-config (filter event-in-filter-list @events))))
 
+(defn month-to-monthname [month]
+  (case month
+    1 "januar"
+    2 "februar"
+    3 "mars"
+    4 "april"
+    5 "mai"
+    6 "juni"
+    7 "juli"
+    8 "august"
+    9 "september"
+    10 "oktober"
+    11 "november"
+    12 "desember"))
+
+(defn date-to-string [date]
+  (let [parsed-date (timec/from-date date)]
+    (str (time/day parsed-date) ". " (month-to-monthname (time/month parsed-date)))
+    ))
+
+
 (defn form-filters []
+  [:div
+   [:div.row.form-filters
+    [:div.col-md-12
+     [:label
+      [:input#only-future {:type           "checkbox"
+                           :defaultChecked (:only-future? @view-configuration)
+                           :on-change      (fn [ev]
+                                             (swap! view-configuration assoc :only-future? (true? (-> ev .-target .-checked)))
+                                             (generate-shown-events @events shown-events))}]
+      "Kun fremtidige stevner"]]]
   [:div.row.form-filters
-   [:label
-    [:input#only-future {:type           "checkbox"
-                         :defaultChecked (:only-future? @view-configuration)
-                         :on-change      (fn [ev]
-                                           (swap! view-configuration assoc :only-future? (true? (-> ev .-target .-checked)))
-                                           (generate-shown-events @events shown-events))}]
-    "Vis kun kommende stevner"]])
+   [:div.col-md-12
+    [:label
+     [:input#only-midtnorge {:type           "checkbox"
+                             :defaultChecked (:only-midtnorge? @view-configuration)
+                             :on-change      (fn [ev]
+                                               (swap! view-configuration assoc :only-midtnorge? (true? (-> ev .-target .-checked)))
+                                               (generate-shown-events @events shown-events))}]
+     "Kun stevner i midtnorge"]]]])
 
 (defn events-page []
   (GET "/api/events" {:handler (fn [incoming]
@@ -46,11 +86,9 @@
           [:div.col-md-12
            [:div.row
             [:div.col-md-12
-             [:h4 (str (:date ev) " - " (:competition ev) " - " (:organizer-full ev))]]]
+             [:h4 (str (date-to-string (:date ev)) " - " (:organizer-full ev))]]]
            [:div.row.last-in-item
             [:div.col-md-3
-             [:span (str (:date ev))]]
-            [:div.col-md-3
              [:span (:competition ev)]]
-            [:div.col-md-6
+            [:div.col-md-9
              [:span (:comments ev)]]]]))]]))
