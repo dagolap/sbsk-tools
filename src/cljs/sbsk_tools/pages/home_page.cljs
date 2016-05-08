4272ced1101f2b19a8d972c47660a04350e3c524
Dag Olav Prestegarden <dagolav@prestegarden.com>
2016-05-09 00:24:25 +0200

    Showing some links on the main page

--- src/cljs/sbsk_tools/pages/home_page.cljs
+++ src/cljs/sbsk_tools/pages/home_page.cljs
@@ -2,8 +2,19 @@
 
 (defn home-page []
   [:div.container
-   [:div.jumbotron
-    [:h1 "Sverresborg Bueskytterverktøy"]
-    [:p "Her finnes diverse hjelpsomme verktøy ifm. bueskyting i Norge."]]
    [:div.row
-    [:div.col-md-12 [:a {:href "#/events"} "Stevner"]]]])
+    [:div.col-md-12
+     [:h3 "Stevner i Norge"]
+     [:ul [:li [:a {:href "#/events"} "Stevner"]]]
+     [:h3 "Eksterne lenker"]
+     [:h4 "Kjøpe bueutstyr"]
+     [:ul
+      [:li [:a {:href "http://www.buesport.no"} "Midt-Norsk Buesport"]]
+      [:li [:a {:href "http://www.bueutstyr.no"} "Humlekjær Archery"]]
+      [:li [:a {:href "http://eriksensport.no"} "Eriksen Sport"]]
+      [:li [:a {:href "http://www.pilogbue.com/"} "Catos Buesport"]]]
+     [:h4 "Piler og tuning"]
+     [:ul
+      [:li "Easton tuning guide"]
+      [:li "Easton arrow shaft selector"]
+      [:li "Easton spine calculator"]]]]])
