d97c92aa3329a922dafdc83526a681ceb8139f4a
Dag Olav Prestegarden <dagolav@prestegarden.com>
2016-05-09 23:56:10 +0200

    Updating links on front page

--- src/cljs/sbsk_tools/pages/home_page.cljs
+++ src/cljs/sbsk_tools/pages/home_page.cljs
@@ -4,17 +4,16 @@
   [:div.container
    [:div.row
     [:div.col-md-12
-     [:h3 "Stevner i Norge"]
-     [:ul [:li [:a {:href "#/events"} "Stevner"]]]
+     [:h3 "Våre verktøy"]
+     [:ul [:li [:a {:href "#/events"} "Stevneoversikt"]]]
      [:h3 "Eksterne lenker"]
-     [:h4 "Kjøpe bueutstyr"]
+     [:h5 "Kjøpe bueutstyr"]
      [:ul
       [:li [:a {:href "http://www.buesport.no"} "Midt-Norsk Buesport"]]
       [:li [:a {:href "http://www.bueutstyr.no"} "Humlekjær Archery"]]
       [:li [:a {:href "http://eriksensport.no"} "Eriksen Sport"]]
       [:li [:a {:href "http://www.pilogbue.com/"} "Catos Buesport"]]]
-     [:h4 "Piler og tuning"]
+     [:h5 "Piler og tuning"]
      [:ul
-      [:li "Easton tuning guide"]
-      [:li "Easton arrow shaft selector"]
-      [:li "Easton spine calculator"]]]]])
+      [:li [:a {:href "http://www.eastonarchery.com/downloads/tuning-guide"} "Easton tuning guide"]]
+      ]]]])
