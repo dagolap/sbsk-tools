cc113938e10646f49fd2a3e9855126b124bf2a29
Dag Olav Prestegarden <dagolav@prestegarden.com>
2016-05-12 23:29:19 +0200

    Splitting main page into two columns

--- src/cljs/sbsk_tools/pages/home_page.cljs
+++ src/cljs/sbsk_tools/pages/home_page.cljs
@@ -3,10 +3,11 @@
 (defn home-page []
   [:div.container
    [:div.row
-    [:div.col-md-12
+    [:div.col-md-6
      [:h3 "Våre verktøy"]
-     [:ul [:li [:a {:href "#/events"} "Stevneoversikt"]]]
-     [:h3 "Eksterne lenker"]
+     [:ul [:li [:a {:href "#/events"} "Stevneoversikt"]]]]
+    [:div.col-md-6
+     [:h3 "Eksterne verktøy"]
      [:h5 "Kjøpe bueutstyr"]
      [:ul
       [:li [:a {:href "http://www.buesport.no"} "Midt-Norsk Buesport"]]
